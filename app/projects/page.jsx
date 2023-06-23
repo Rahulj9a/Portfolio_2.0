6acd35b4006fd83cc26628ece04c37626779fdeb
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-06-23 22:01:25 +0530

    add link preview feature

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,31 +1,39 @@
 "use client";
 
 import ProjectCard from "@/components/ProjectCard";
-import useProjectStore from "@/store/projectStore";
+ 
 import axios from "axios";
 import React from "react";
 import { useQuery } from "react-query";
+import LinkPreview from "@/components/linkPreview";
 
 const Projects = () => {
-  const { projects, setProjects } = useProjectStore();
-  const { data, isLoading, error } = useQuery("projects", async () => {
-    const response = await axios.get("/data/projects.json");  
-    
-     
-    
+  
+  const { data:projectData, isLoading:projectLoading, error:projectError } = useQuery("projects", async () => {
+    const response = await axios.get("/data/projects.json");
+
     return response.data;
   });
-   
+  const { data:blogData, isLoading:blogLoading, error:blogError } = useQuery("blogs", async () => {
+    const response = await axios.get("/data/blogs.json");
+
+    return response.data;
+  });
+
   return (
     <div className="flex flex-col justify-evenly m-auto md:left-20 left-0 right-0 min-h-full h-fit   bg-custom2 pt-10  absolute">
       <section id="CodingProjects" className="w-full h-auto">
         <h2 className=" sticky top-10 w-full h-8 my-5 bg-custom1 text-xl  text-center text-custom4">
           Projects
         </h2>
-        <div id="projectItems" className="flex flex-wrap gap-8 w-auto justify-evenly">
-
-              {isLoading?"Loading...":data.map((project, index)=>{
-                return (<ProjectCard key={index} data={project} />)
+        <div
+          id="projectItems"
+          className="flex flex-wrap gap-8 w-auto justify-evenly"
+        >
+          {projectLoading
+            ? "Loading..."
+            : projectData.map((project, index) => {
+                return <ProjectCard key={index} data={project} />;
               })}
         </div>
       </section>
@@ -34,7 +42,12 @@ const Projects = () => {
           Blogs
         </h2>
         <div className="flex flex-wrap gap-y-8 w-full  justify-evenly">
-           {/* projectCard */}
+        {blogLoading
+            ? "Loading..."
+            : blogData.map((blog, index) => {
+                return <LinkPreview url={blog.url}/>;
+              })}
+          
         </div>
       </section>
     </div>
---
+++ components/linkPreview.jsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+function LinkPreview({ url, width = "250px", height = "150px" }) {
+  const [previewData, setPreviewData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter()
+
+  const handleClick = (e) => {
+    window.open(url, '_blank');
+
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, "text/html");
+        const title = doc.querySelector("title")?.textContent || "";
+        const description =
+          doc
+            .querySelector('meta[name="description"]')
+            ?.getAttribute("content") || "";
+        const image =
+          doc
+            .querySelector('meta[property="og:image"]')
+            ?.getAttribute("content") || "";
+
+        setPreviewData({ title, description, image });
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!previewData) {
+    return <p>Failed to fetch link preview.</p>;
+  }
+
+  return (
+    <div onClick={handleClick} className="cursor-pointer">
+      <div className="projectCard md:w-[300px] md:h-[300px] w-[250px] h-[2500px] rounded-2xl hover:shadow-circle-shadowSmall hover:shadow-custom4">
+        <div className="h-2/3 w-full bg-custom1 rounded-t-2xl flex items-center justify-center overflow-hidden">
+          <img src={previewData.image} className=" w-auto h-auto bg-contain" />
+        </div>
+        <div className="h-1/3 bg-custom1 border-custom2 border-t-2 w-full flex items-center justify-center  rounded-b-2xl">
+          <p className="text-custom4 text-center">{previewData.title}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LinkPreview;
