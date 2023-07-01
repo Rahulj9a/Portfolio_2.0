b7f0a5150e1dea22d14c66ff55199d763387522e
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-07-01 19:13:39 +0530

    fix height issue of projectCard  making it responsive

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,27 +1,49 @@
 import Image from "next/image";
 import React from "react";
+import { MdOutlinePreview } from "react-icons/md";
+import { BsGithub } from "react-icons/bs";
+import Link from "next/link";
 
 const ProjectCard = (data) => {
-  const { title, image, description, techs } = data.data;
-  console.log(title);
+  const { title, image, description, techs, links } = data.data;
+   
+  
 
   return (
-    <div className="projectCard md:w-[90%] md:h-[350px] flex  h-[300px] rounded-2xl  shadow-circle-shadowSmall hover:shadow-custom4">
-      <div className="h-full w-full   rounded-l-2xl  flex items-center justify-center overflow-hidden">
-        <Image
-          src={image}
-          height={500}
-          width={500}
-          className=" w-auto h-auto bg-contain"
-        />
+    <div className="projectCard   relative  w-[90%]   h-auto flex rounded-2xl shadow-circle-shadowSmall hover:shadow-custom4">
+      <div className={` hover:opacity-100  hover:bg-opacity-80 opacity-0 flex rounded-2xl z-10 bg-custom4   h-full w-full absolute items-center justify-center  `}>
+        <div className="flex gap-10">
+          {links.map((link)=>{
+             
+              return(
+                <Link href={link}> {link.includes('github')?<BsGithub size={50}/>:<MdOutlinePreview size={50}/>} </Link>
+              )
+            
+          })}
+        </div> 
       </div>
-      <div className="h-full relative w-full flex flex-col">
-        <div className="h-4/5 w-full  flex flex-col justify-center items-center   rounded-tr-2xl ">
-          <h2 className="text-custom4 font-bold text-xl" >{title}</h2>
-          <p className="text-custom4">{description}</p>
+
+      <div className="md:flex-row flex-col flex h-auto   w-full  items-center justify-arounds ">
+        <div className="h-auto   w-full   rounded-2xl flex-col  flex items-center justify-around ">
+          <Image
+           
+            src={image}
+            height={400}
+            width={400}
+            className=" rounded-2xl h-auto  w-auto  bg-contain"
+          />
+          
         </div>
-        <div className="h-1/5 w-full  border-t-2 border-custom2 rounded-br-2xl  flex items-center justify-evenly overflow-hidden ">
-          {techs.map((tech,index)=>{return (<span className="text-custom3 "> {tech} </span>)})}
+        <div className="md:h-full  md:flex-auto h-auto  justify-around  w-full flex flex-col">
+          <div className="  h-auto w-full  flex flex-col justify-center items-center   rounded-tr-2xl ">
+            <h2 className="text-custom4 font-bold text-xl h-auto">{title}</h2>
+            <p className="text-custom4  h-auto">{description}</p>
+          </div>
+          <div className="  w-full  flex border-t-2 border-custom2 rounded-br-2xl  items-center justify-evenly overflow-hidden ">
+            {techs.map((tech, index) => {
+              return <span className="text-custom3 "> {tech} </span>;
+            })}
+          </div>
         </div>
       </div>
     </div>
--- components/SkillSection.jsx
+++ components/SkillSection.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const SkillSection = ({ skillName, skillIcon, children }) => {
   return (
-    <div id={`${skillName}Corner`} className="h-auto flex w-full px-5 min-h-[80px]">
+    <div id={`${skillName}Corner`} className="h-auto flex w-full md:text-base text-sm px-5 min-h-[80px]">
        
         <div
           id="skillName"
-          className="flex items-center justify-end pr-3 text-custom4  w-2/12"
+          className="flex items-center justify-center md:pr-3 text-custom4  md:w-2/12 w-3/12"
         >
           <p>{skillName}</p>
         </div>
@@ -15,7 +15,7 @@ const SkillSection = ({ skillName, skillIcon, children }) => {
           className="flex items-center justify-center relative w-1/12"
         >
           <div className="absolute h-full w-[2px] bg-custom3"></div>
-          <div className="absolute h-7 w-7 border-2 shadow-circle-shadowSmall hover:shadow-circle-shadow hover:shadow-custom4 shadow-custom4 border-custom3 rounded-full bg-custom2 z-10">
+          <div className="absolute md:h-7 md:w-7 w-3 h-3 border-2 shadow-circle-shadowSmall hover:shadow-circle-shadow hover:shadow-custom4 shadow-custom4 border-custom3 rounded-full bg-custom2 z-10">
             {skillIcon || ""}
           </div>
         </div>
